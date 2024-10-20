a5c33d7f0347f7349a33ffe6a03299d507c690b3
nitinkapoor7 <nitinkapoor117@gmail.com>
2024-10-21 01:44:57 +0530

    New project ongoing

--- src/Comp/Header.jsx
+++ src/Comp/Header.jsx
@@ -3,47 +3,89 @@ import Fab from '@mui/material/Fab';
 import ArticleIcon from '@mui/icons-material/Article';
 import WavingHandIcon from '@mui/icons-material/WavingHand';
 
+const handleDownload = () => {
+  window.open("https://drive.google.com/uc?export=download&id=1pGCl5-msrwhx8ynl7nhhvs_I3beFY1wD", "_blank");
+};
+
 const Header = ({ onSectionClick }) => {
   return (
     <div className="container" style={{ fontFamily: "-moz-initial" }}>
-      <div className="">
-        <div className="col-md-10 mx-5">
-          <div className="card" style={{ padding: '57px', marginLeft: '10px', border: '1px solid #ddd', boxShadow: '0 0 10px rgba(0,0,0,0.1)' }}>
-            <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-              <img src={`${process.env.PUBLIC_URL}/mein1.jpg`} alt="Your image" style={{ width: '120px', height: '118px', borderRadius: '100%' }} />
-            </div>
-            <p style={{ fontSize: "3rem", fontWeight: "bold", marginBottom: '5px' }}>
-              Nitin Kapoor
+      <div className="col-md-10 mx-5">
+        <div
+          className="card"
+          style={{
+            padding: "57px",
+            marginLeft: "10px",
+            border: "1px solid #ddd",
+            boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+          }}
+        >
+          <div style={{ textAlign: "center", marginBottom: "20px" }}>
+            <img
+              src={`${process.env.PUBLIC_URL}/mein1.jpg`}
+              alt="Your image"
+              style={{
+                width: "110px",
+                height: "108px",
+                borderRadius: "100%",
+              }}
+            />
+          </div>
+          <p
+            style={{
+              fontSize: "3rem",
+              fontWeight: "bold",
+              marginBottom: "6px",
+            }}
+          >
+            Nitin Kapoor
+          </p>
+          <p style={{ fontSize: "19px", marginTop: "5px", marginLeft: "2px" }}>
+            Frontend Developer | BCA '25{" "}
+            <span style={{ fontSize: "15px" }}>@MDU</span>
+          </p>
+          <div className="my-2">
+            <Fab
+              variant="extended"
+              color="primary"
+              onClick={handleDownload}
+              sx={{ mr: 2 }}
+            >
+              <ArticleIcon sx={{ mr: 1 }} />
+              Resume
+            </Fab>
+            <Fab
+              variant="extended"
+              color="primary"
+              onClick={() => onSectionClick("sayHi")}
+            >
+              <WavingHandIcon sx={{ mr: 1 }} />
+              Say Hi
+            </Fab>
+          </div>
+          <hr />
+          <div
+            style={{
+              fontSize: "17px",
+              textAlign: "left",
+              fontFamily: "sans-serif",
+              lineHeight: "1.5",
+            }}
+          >
+            <p>
+              I am a passionate Frontend Developer in my final year of pursuing
+              a Bachelor of Computer Applications. Specializing in crafting
+              visually appealing and user-friendly web applications with ReactJS
+              and Material-UI. Some of my likings are playing football and
+              writing blogs about any topic. I am eager to secure an internship
+              to refine my skills further and contribute to exciting projects in
+              the tech industry.
             </p>
-            <p style={{ fontSize: '19px', marginTop: '5px', marginLeft: '2px' }}>
-              Frontend Developer | BCA '25 <span style={{ fontSize: '15px' }}>@MDU</span>
-            </p>
-            <div className="my-3">
-              <Fab variant="extended" color="primary" sx={{ mr: 2 }}>
-                <ArticleIcon sx={{ mr: 1 }} />
-                Resume
-              </Fab>
-              <Fab variant="extended" color="primary" onClick={() => onSectionClick('sayHi')}>
-                <WavingHandIcon sx={{ mr: 1 }} />
-                Say Hi
-              </Fab>
-            </div>
-            <hr />
-            <div style={{ fontSize: '17px', textAlign: 'left', fontFamily: 'sans-serif', lineHeight: '1.5'}}>
-  <p>
-    I am a passionate Frontend Developer in my final year of pursuing a 
-    Bachelor of Computer Applications. Specializing in crafting 
-    visually appealing and user-friendly web applications with 
-    ReactJS and Material-UI. Some of my likings are playing football and writing blogs about any topic. I am eager to secure an internship 
-    to refine my skills further and contribute to exciting 
-    projects in the tech industry.
-  </p>
-</div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
--- src/Comp/Projects.jsx
+++ src/Comp/Projects.jsx
@@ -1,50 +1,56 @@
+import './Project.css';
+
 const Projects = () => {
   return (
-    <div style={styles.cardProject}>
-      <div style={styles.flexContainer}>
-        <a
-          href="https://youtu.be/DMnZTRnDkeg?si=vCxDdsbWNGmCMPpw"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src={`${process.env.PUBLIC_URL}/image.png`}
-            alt="Thumbnail"
-            style={styles.image}
-          />
-        </a>
-        <div style={styles.textContainer}>
-          <h5>Citrus Fruits</h5>
-          <p>
-            Citrus Fruits is a platform that helps local fruit and vegetable sellers deliver fresh produce directly to customers’ doorsteps, providing a seamless experience for both vendors and consumers looking for high-quality, farm-fresh products.
-          </p>
+    <>
+      <div className="cardProject">
+        <div className="flexContainer">
+          <a
+            href="https://youtu.be/DMnZTRnDkeg?si=vCxDdsbWNGmCMPpw"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={`${process.env.PUBLIC_URL}/image.png`}
+              alt="Thumbnail"
+              className="image"
+            />
+          </a>
+          <div className="textContainer">
+            <h5>Citrus Fruits</h5>
+            <p>
+              Citrus Fruits is a platform that helps local fruit and vegetable
+              sellers deliver fresh produce directly to customers’ doorsteps,
+              providing a seamless experience for both vendors and consumers
+              looking for high-quality, farm-fresh products.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
 
-const styles = {
-  cardProject: {
-    backgroundColor: "#fff",
-    padding: "20px",
-    margin: "9px",
-    borderRadius: "8px",
-    boxShadow: "0 10px 8px rgba(0, 0, 0, 0.1)",
-  },
-  flexContainer: {
-    display: 'flex',
-    alignItems: 'center',
-  },
-  image: {
-    width: '130px',
-    height: '120px',
-    borderRadius: '50%',
-    marginRight: '20px',
-  },
-  textContainer: {
-    flex: 1,
-  },
+      <div className="cardProject">
+        <div className="flexContainer">
+          <a href="" target="_blank" rel="noopener noreferrer">
+            <img
+              src={`${process.env.PUBLIC_URL}/image1.png`}
+              alt="Thumbnail"
+              className="image"
+            />
+          </a>
+          <div className="textContainer">
+            <h5>Indian Football Team</h5>
+            <p>
+              I built this landing page for the All India Football Federation
+              (AIFF) to help connect fans with the world of Indian football,
+              fostering national pride and community support. The site helps
+              engage a larger audience, making it easy for fans to stay updated
+              on team news, and player updates.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
